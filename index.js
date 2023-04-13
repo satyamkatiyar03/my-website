29717fb31ac6c946037220cb1209b9f959cd8ac4
Satyam Katiyar <51047993+satyamkatiyar03@users.noreply.github.com>
2023-04-13 22:43:05 +0530

    first
    update 2.0

---
+++ index.js
@@ -0,0 +1,19 @@
+var menu = document.getElementById("menu");
+var Nav = document.getElementById("nav");
+var cut = document.getElementById("cut");
+
+menu.addEventListener("click", ()=>{
+    menu.classList.toggle("hide");
+    Nav.classList.toggle("hide");
+    cut.classList.toggle("hide");
+})
+cut.addEventListener("click", ()=>{
+    menu.classList.toggle("hide");
+    Nav.classList.toggle("hide");
+    cut.classList.toggle("hide");
+})
+
+
+
+
+
